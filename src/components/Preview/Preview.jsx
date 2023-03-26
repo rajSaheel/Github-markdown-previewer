ebb851d3a3b176c2227f1e691832bb0b4f7afdf1
Saheel Raj <89908818+rajSaheel@users.noreply.github.com>
2023-03-26 07:19:39 +0000

    fixed sanitizing & test cases passed

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,17 +1,24 @@
 import { useSelector } from "react-redux"
 import { marked } from "marked"
+import DOMPurify from 'dompurify'
+import insane from "insane"
 
 export const Preview = () => {
+
   const markup = useSelector(state => state.markup)
   marked.setOptions({
-    breaks:true,
-    sanitise:true
+    breaks: true,
   })
+
+  const dirty = marked.parse(markup)
 
   return (
     <div id={"preview-area"}>
       <div id={"preview-toolbar"}><h3>Preview</h3></div>
-      <div id={"preview"}>{marked.parse(markup)}</div>
+      <div id={ "preview"}dangerouslySetInnerHTML={{ __html: dirty }} />
+      {/*       <div id={"preview"}>{DOMPurify.sanitize(dirty)}</div> */}
+      {/*       <div id={"preview"}><>{sanitize(dirty)}</></div> */}
+
     </div>
   )
 }
--- src/store/markup/markup.js
+++ src/store/markup/markup.js
@@ -9,7 +9,26 @@ export const editMarkup = (newMarkup) => {
   }
 }
 
-const defaultState = "Default Markup"
+const defaultState =
+  `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:Heres some code, \`<div></div>\`, between 2 backticks.
+There's also [links](https://www.freecodecamp.org), and
+There is a **bold text**
+> Block Quotes!
+\`\`\`
+  function sayHello(){
+    hehe
+  }
+\`\`\`
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
 
 const markup = (state = defaultState, action) => {
   switch (action.type) {
