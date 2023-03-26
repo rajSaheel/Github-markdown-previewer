41c44c554c64a00823eb6eecd09fd0ec63b47541
Saheel Raj <89908818+rajSaheel@users.noreply.github.com>
2023-03-26 07:58:34 +0000

    style done

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,6 +1,9 @@
 import "./Editor.css"
 import { useSelector, useDispatch } from "react-redux"
 import { editMarkup } from "../.././store/markup/markup.js"
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons'
 
 export const Editor = () => {
 
@@ -8,11 +11,11 @@ export const Editor = () => {
   const dispatch = useDispatch()
 
   return (
-    <div id={"editor-box"}>
-      <div id={"tool-bar"}>
-        <h3>Editor</h3>
+    <div id={"editor-wrap"}>
+      <div className={"tool-bar"}>
+        <FontAwesomeIcon icon={faNewspaper} /><h3>Editor</h3>
       </div>
-      <textarea id={"editor"} value={markup} onChange={(e) => dispatch(editMarkup(e.target.value))} />
+      <textarea id={"editor"} type={"text"} value={markup} onChange={(e) => dispatch(editMarkup(e.target.value))} />
     </div>
   )
 }
--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import { marked } from "marked"
-import DOMPurify from 'dompurify'
-import insane from "insane"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 
 export const Preview = () => {
 
@@ -13,12 +13,12 @@ export const Preview = () => {
   const dirty = marked.parse(markup)
 
   return (
-    <div id={"preview-area"}>
-      <div id={"preview-toolbar"}><h3>Preview</h3></div>
-      <div id={ "preview"}dangerouslySetInnerHTML={{ __html: dirty }} />
+    <div id={"preview-wrap"}>
+      <div className={"tool-bar"}><FontAwesomeIcon icon={faMagnifyingGlass} /><h3>Preview</h3></div>
+      <div id={"preview"} dangerouslySetInnerHTML={{ __html: dirty }} />
       {/*       <div id={"preview"}>{DOMPurify.sanitize(dirty)}</div> */}
       {/*       <div id={"preview"}><>{sanitize(dirty)}</></div> */}
 
-    </div>
+    </div >
   )
 }
